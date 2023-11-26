6b17402d68d0aa9468af0bd0969e3a5e07cfbe36
khattab88 <m_khatab_88@hotmail.com>
2023-11-26 20:12:57 +0200

    override mapping (ignore())

--- API/Dtos/Response/TeamDto.cs
+++ API/Dtos/Response/TeamDto.cs
@@ -1,5 +1,6 @@
 ﻿using API.Mappings;
 using API.Models;
+using AutoMapper;
 
 namespace API.Dtos.Response
 {
@@ -8,5 +9,13 @@ namespace API.Dtos.Response
         public int Id { get; set; }
         public string Name { get; set; }
         public string Country { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Team, TeamDto>()
+                .ForMember(dest => dest.DateUpdated, opt => opt.Ignore());
+        }
     }
 }
